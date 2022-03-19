142384142c31135e25e13cfd0d2bf9558db42efe
task11 <task11@naver.com>
2022-03-19 23:53:52 +0900

    feat: real-time clock 0.2

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,49 +1,24 @@
-class clock {
+const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+const templateId = document.getElementById('root')
 
+export default class Clock {
+  render = () => {
+    const curr = new Date();
+    const utc = curr.getTime() + curr.getTimezoneOffset() * 60 * 1000;
+    const today = new Date(utc + (KR_TIME_DIFF));
 
-
-  constructor() {
-    this.$clockTitle = document.querySelector(".js-clock");
-    this.D_DAY = "12/25";
-    getDday();
-    setInterval(getDday, 1000);
+    const dayObject = {
+      year: today.getFullYear(),
+      month: String(today.getMonth() + 1).padStart(2, "0"),
+      date: String(today.getDate()).padStart(2, "0"),
+      hour: String(today.getHours()).padStart(2, "0"),
+      min: String(today.getMinutes()).padStart(2, "0"),
+      sec: String(today.getSeconds()).padStart(2, "0")
+    }
+    templateId.innerHTML = `
+                <div class="real-time-clock">
+                    <span>${dayObject.year}-${dayObject.month}-${dayObject.date}  ${dayObject.hour} : ${dayObject.min} : ${dayObject.sec} </span>
+                </div>
+            `;
   }
-
-
-
-  getDate = (date) => {
-    return {
-      year: date.getFullYear(),
-      time: date.getTime()
-    };
-  };
-
-  compareDate = (now, dDay) => {
-    const gap = dDay.time - now.time;
-
-    return {
-      day: String(Math.floor(gap / (1000 * 60 * 60 * 24))).padStart(3, "0"),
-      hour: String(Math.floor((gap / (1000 * 60 * 60)) % 24)).padStart(2, "0"),
-      min: String(Math.floor((gap / (1000 * 60)) % 60)).padStart(2, "0"),
-      sec: String(Math.floor((gap / 1000) % 60)).padStart(2, "0")
-    };
-  };
-
-  getDday = () => {
-    const current = new Date();
-    const currentDate = getDate(current);
-
-    const christmas = new Date(`${currentDate.year}/${D_DAY}`);
-    const christmasDate = getDate(christmas);
-
-    const dDate = compareDate(currentDate, christmasDate);
-
-    $clockTitle.innerText = `${dDate.day}d ${dDate.hour}h ${dDate.min}m ${dDate.sec}s`;
-  };
-
-
-
-
 }
-
-export default clock;
